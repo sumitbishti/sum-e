0dc709376df16da3bfc186a348fb72658930e54b
Sumit Bisht <sumit7beazt@gmail.com>
2023-04-18 21:15:45 +0530

    url changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 		setLoading(true);
 
 		try {
-			const response = await fetch("https://sum-e.vercel.app//api/v1/post", {
+			const response = await fetch("https://sum-e.vercel.app/api/v1/post", {
 				method: "GET",
 				headers: {
 					"Content-Type": "application/json",
