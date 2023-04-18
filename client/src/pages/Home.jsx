71877ffa431129205479f778e3bfc4f58609042f
Sumit Bisht <sumit7beazt@gmail.com>
2023-04-18 21:39:58 +0530

    updated server url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,12 +22,15 @@ const Home = () => {
 		setLoading(true);
 
 		try {
-			const response = await fetch("https://sum-e.vercel.app/api/v1/post", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
+			const response = await fetch(
+				"https://sum-e-server.vercel.app/api/v1/post",
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
 			if (response.ok) {
 				const result = await response.json();
